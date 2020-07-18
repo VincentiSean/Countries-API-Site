275e09ff7d90773cc669eb1a9e01c63e9402ce94
VincentiSean <vincenti.sean@gmail.com>
2020-07-18 10:41:20 -0400

    changed select to custom drop down to edit look without hooks

--- countries/src/components/countries.js
+++ countries/src/components/countries.js
@@ -1,9 +1,82 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom';
+// import styled from 'styled-components';
+
+// const DropDownContainer = styled("div")`
+//     background: var(--white);
+//     box-sizing: border-box;
+//     font-family: 'Nunito Sans', sans-serif;
+//     margin-top: 20px;
+//     margin-left: 5vw;
+//     width: 50vw;
+// `;
+
+// const DropDownHeader = styled("div")`
+//     background-color: var(--white);
+//     border-radius: 5px;
+//     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+//     color: var(----dark-blue-lmt);
+//     font-size: 14px;
+//     font-weight: 400;
+//     padding: 10px;
+// `;
+
+// const DropDownListContainer = styled("div")``;
+
+// const DropDownList = styled("ul")`
+//     background-color: var(--white);
+//     border-radius: 5px;
+//     box-sizing: border-box;
+//     color: var(--dark-blue-lmt);
+//     font-size: 14px;
+//     font-weight: 400;
+//     margin: 0;
+//     padding: 0;
+// `;
+
+// const ListItem = styled("li")`
+//     list-style: none;
+//     margin-bottom: 10px;
+// `;
 
 class Countries extends Component {
-    state = {
-        countries: []
+    constructor() {
+        super();
+
+        this.state = {
+            countries: [],
+            showMenu: false,
+        }
+    
+        this.showMenu = this.showMenu.bind(this);
+        this.closeMenu = this.closeMenu.bind(this);
+    }
+    
+
+    showMenu(event) {
+        event.preventDefault();
+
+        this.setState({ showMenu: true }, () => {
+            document.addEventListener('click', this.closeMenu);
+        });
+    }
+
+    closeMenu(event) {
+        if (this.africa.contains(event.target)) {
+            this.selectFilter("africa");
+        } else if (this.america.contains(event.target)) {
+            this.selectFilter("americas");
+        } else if (this.asia.contains(event.target)) {
+            this.selectFilter("asia");
+        } else if (this.europe.contains(event.target)) {
+            this.selectFilter("europe");
+        } else if (this.oceania.contains(event.target)) {
+            this.selectFilter("oceania");
+        }
+
+        this.setState({ showMenu: false }, () => {
+            document.removeEventListener('click', this.closeMenu);
+        });
     }
     
     componentDidMount() {
@@ -20,10 +93,8 @@ class Countries extends Component {
     }
 
     selectFilter(region) {
-        console.log(region.target.value)
-        let url = "https://restcountries.eu/rest/v2/region/"+region.target.value
+        let url = "https://restcountries.eu/rest/v2/region/"+region
         this.getCountries(url)
-        console.log(url)
     }
 
     handleChange(input) {
@@ -45,23 +116,81 @@ class Countries extends Component {
     }
 
     render() {
+        
+
         if (Array.isArray(this.state.countries)) {
             return(
-                <div>
+                <div id="countries-container">
                     <section id="search-section">
                         <input 
                             type="text" 
                             name="countrySearch" 
                             placeholder="Search for a country..."
                             onChange={(e) => this.handleChange(e)} />
-                        <select name="regionFilter" onChange={(e) => this.selectFilter(e)}>
-                            <option value="" disabled selected hidden>Filter by Region</option>
-                            <option value="Africa">Africa</option>
-                            <option value="Americas">Americas</option>
-                            <option value="Asia">Asia</option>
-                            <option value="Europe">Europe</option>
-                            <option value="Oceania">Oceania</option>
-                        </select>
+                        <button onClick={this.showMenu}>Filter by region</button>
+                        {
+                            this.state.showMenu ? (
+                            <div 
+                                className="menu"
+                                ref={(element) => {
+                                    this.dropdownMenu = element;
+                                }}>
+                                <button ref={(element) => {
+                                    this.africa = element;
+                                }}>
+                                    Africa
+                                </button>
+                                <button ref={(element) => {
+                                    this.america = element;
+                                }}>
+                                    Americas
+                                </button>
+                                <button ref={(element) => {
+                                    this.asia = element;
+                                }}>
+                                    Asia
+                                </button>
+                                <button ref={(element) => {
+                                    this.europe = element;
+                                }}>
+                                    Europe
+                                </button>
+                                <button ref={(element) => {
+                                    this.oceania = element;
+                                }}>
+                                    Oceania
+                                </button>
+                            </div>
+                            )
+                            : (
+                                null
+                            )
+                        }
+                        
+                        {/* <DropDownContainer>
+                            <DropDownHeader onClick={toggling}>Filter by Region</DropDownHeader>
+                            {isOpen && (
+                                <DropDownListContainer>
+                                    <DropDownList>
+                                        <ListItem>Africa</ListItem>
+                                        <ListItem>Americas</ListItem>
+                                        <ListItem>Asia</ListItem>
+                                        <ListItem>Europe</ListItem>
+                                        <ListItem>Oceania</ListItem>
+                                    </DropDownList>
+                                </DropDownListContainer>
+                            )}
+                        </DropDownContainer> */}
+                        {/* <div id="custom-select">
+                            <select name="regionFilter" onChange={(e) => this.selectFilter(e)}>
+                                <option value="" disabled selected hidden>Filter by Region</option>
+                                <option value="Africa">Africa</option>
+                                <option value="Americas">Americas</option>
+                                <option value="Asia">Asia</option>
+                                <option value="Europe">Europe</option>
+                                <option value="Oceania">Oceania</option>
+                            </select>
+                        </div> */}
                     </section>
                     {this.state.countries.map((country) => (
                         <Link 
